f59b9c86b581fd56cb769984a60a4ec0ffe13632
Alton Michaux <alton.michaux@yahoo.com>
2020-12-14 22:34:01 -0500

    created for loops and assigned ships to coordinates, as well as add to ship count. Slowly making my way through this.

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const battleship = () => {
-  return 'The winner is...?'
-}
+  let player1Name = prompt("What is your name?");
+  let player2Name = prompt("What is your name?");
+  let players = [
+    {
+      name: player1Name,
+      shipCount: 0,
+      gameBoard: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ],
+    },
+    {
+      name: player2Name,
+      shipCount: 0,
+      gameBoard: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ],
+    },
+  ];
 
-const gameResult = battleship()
+  for (i = 0; i < 4; i++) {
+    let randX = Math.floor(Math.random() * 3) + 1;
+    let randY = Math.floor(Math.random() * 3) + 1;
+    players[0].gameBoard[i] = [randX, randY];
+    players[1].gameBoard[i] = [randX, randY];
+    for (j = 0; j < 2; j++) {
+      if ([randX, randY] !== players[j].gameBoard[i]) {
+        players[j].shipCount += 1;
+        console.log(players[j]);
+      }
+    }
+  }
+  
+  // let strike = parseInt(
+  //   prompt("Choose an x and y coordinate to strike your opponent's battleship.")
+  // );
 
-const htmlTarget = document.getElementById('result')
-htmlTarget.innerHTML = gameResult
+  // return "The winner is...?";
+};
+
+const gameResult = battleship();
+
+const htmlTarget = document.getElementById("result");
+htmlTarget.innerHTML = gameResult;
