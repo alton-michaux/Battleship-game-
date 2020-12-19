c5de35af573c689a827fed01d326208ccf507d64
Alton Michaux <alton.michaux@yahoo.com>
2020-12-19 09:13:14 -0500

    consolidated js files and updated html file to point back to original js file

--- index.js
+++ index.js
@@ -1,89 +1,150 @@
 const battleship = () => {
-  let p1Name = prompt("What is your name Player 1?");
-  let p2Name = prompt("What is your name Player 2?");
+  //assign name variables
+  let p1 = prompt("What is your name P1?");
+  let p2 = prompt("What is your name P2?");
+  //assign winner variable
+  let winner;
 
+  //declare grid variable for p1
   let p1Grid = createGrid(4);
+  //call function for random ship placement on p1 grid
+  placeCharacters(1, p1Grid, 4);
+  console.log(p1Grid)
+  //declare grid variable for p2
   let p2Grid = createGrid(4);
+  //call function for random ship placement on p2 grid
+  placeCharacters(1, p2Grid, 4);
+  console.log(p2Grid)
 
+  //initialize players objects
   const players = [
     {
-      name: p1Name,
-      shipCount: 0,
-      gameBoard: p1Grid,
+      name: p1,
+      shipCount: 4,
+      gameBoard: p1Grid, //set up 2D array to make 4x4 grid
     },
     {
-      name: p2Name,
-      shipCount: 0,
+      name: p2,
+      shipCount: 4,
       gameBoard: p2Grid,
     },
   ];
 
-  //write a function to create grid
+  //setup functions
+
+  //1. create function that takes a size parameter to create a grid
   function createGrid(size) {
-    let grid = []; //initialize grid array
+    let grid = [];
     for (i = 0; i < size; i++) {
-      grid[i] = []; //sets an array within an array
+      grid[i] = [];
       for (j = 0; j < size; j++) {
-        grid[i][j] = " 0 "; //assigns 0 to all grid spaces
+        grid[i][j] = " 0 ";
       }
     }
+    //return grid value
     return grid;
-  } 
-
-  let grid = createGrid(4);
-
-  //write a loop to populate ships on grid
-    for (i = 0; i < grid.length; i++) {
-      let randX = Math.floor(Math.random() * 3) + 1;
-      let randY = Math.floor(Math.random() * 3) + 1; //generate random coordinates
-      for (j = 0; j < players.length; j++) {
-        if ([randY, randX] !== players[j].gameBoard[i]) { //conditional statement making sure there are no ships on the board
-          players[j].gameBoard[i] = "1 "; //marks ship on player grid
-          players[j].shipCount ++ //adds ship to shipCount variable
-          console.log(players[j]);
-        } else {
-        }
-      }
-    }
-
-  console.log(p1Grid);
-  console.log(p2Grid);
-
-  // console.log(printGrid(p1Grid));
-
-  console.log(printGrid(p1Grid));
-  console.log(printGrid(p2Grid, true));
-
-  function printGrid(grid, isEnemy = false) {
-    const headers = createHeaders(grid.length);
-    // console.log(headers);
-    let rowStr;
-    for (let i = 0; i < grid.length; i++) {
-      rowStr = i + "  ";
-      for (let cell of grid[i]) {
-        if (isEnemy && cell == " 1 ") {
-          rowStr += " 0 ";
-        } else {
-          rowStr += cell + " 0 ";
-        }
-      }
-    }
-    // console.log(rowStr);
-    return rowStr
   }
 
-  function createHeaders(size) {
-    let result = " ";
-    for (i = 0; i < size; i++) {
-      result += i + " ";
+  //2. create a function to generate random coordinates for ships and place them as 1's on the grid
+  function placeCharacters(c, grid, max) {
+    for (i = 0; i < max; i++) {
+      let randX = Math.floor(Math.random() * 4);
+      let randY = Math.floor(Math.random() * 4);
+      if (grid[randX][randY] !== " 1 ") {
+        grid[randX][randY] = ` ${c} `;
+      }
     }
-    return result;
   }
 
-  // return "The winner is...?";
+  //3. create a strike function to pass coordinates through opponent grid
+  function strike(objV, player, opPlayer) {
+    let x = parseInt(
+      prompt(
+        `${objV[player].name} choose an X coordinate to strike ${objV[opPlayer].name}'s battleship (0-3).`
+      )
+    );
+    let y = parseInt(
+      prompt(
+        `${objV[player].name} choose a Y coordinate to strike ${objV[opPlayer].name}'s battleship (0-3).`
+      )
+    );
+    if (objV[opPlayer].gameBoard[x][y] == " 1 ") {
+      alert(
+        `Direct hit ${objV[player].name}! You sank ${
+          objV[opPlayer].name
+        }'s battleship. They have ${
+          objV[opPlayer].shipCount - 1
+        } battleships left`
+      );
+      objV[opPlayer].gameBoard[x][y] = " ! ";
+      objV[opPlayer].shipCount--;
+      return true
+    } else if (
+      objV[opPlayer].gameBoard[x][y] == " - " ||
+      objV[opPlayer].gameBoard[x][y] == " ! "
+    ) {
+      alert(`That spot has already been hit!`);
+      return false
+    } else {
+      alert(`You missed! ${objV[opPlayer].name}'s ships are evading you!`);
+      objV[opPlayer].gameBoard[x][y] = " - ";
+      return false
+    } 
+}
+
+  //4. create function to start gameplay
+  function start(objV, player, opPlayer) {
+    while (objV[player].shipCount > 0 && objV[opPlayer].shipCount > 0) {
+      let attack = strike(objV, player, opPlayer);
+      if (attack === false) {
+        attack = strike(objV, opPlayer, player);
+      } 
+    }
+    if (objV[player].shipCount === 0) {
+      winner = objV[opPlayer].name;
+    } else {
+      if (objV[opPlayer].shipCount === 0) {
+        winner = objV[player].name;
+      }
+      return winner;
+  }
+}
+
+  //call function to start the game
+  start(players, 0, 1);
+
+  console.log(players);
+
+  return `The winner is ${winner}!`
 };
 
 const gameResult = battleship();
 
-const htmlTarget = document.getElementById("result");
+htmlTarget = document.getElementById("result");
 htmlTarget.innerHTML = gameResult;
+
+// function printGrid(grid, isEnemy = false) {
+//   const headers = createHeaders(grid.length);
+//   // console.log(headers);
+//   let rowStr;
+//   for (let i = 0; i < grid.length; i++) {
+//     rowStr = i + "  ";
+//     for (let cell of grid[i]) {
+//       if (isEnemy && cell == " 1 ") {
+//         rowStr += " 0 ";
+//       } else {
+//         rowStr += cell + " 0 ";
+//       }
+//     }
+//   }
+//   // console.log(rowStr);
+//   return rowStr
+// }
+
+// function createHeaders(size) {
+//   let result = " ";
+//   for (i = 0; i < size; i++) {
+//     result += i + " ";
+//   }
+//   return result;
+// }
