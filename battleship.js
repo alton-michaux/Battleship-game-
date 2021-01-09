3b8b15b70d9ceb041224d8e85ae84f5faafa319e
Alton Michaux <alton.michaux@yahoo.com>
2021-01-08 20:02:24 -0500

    tweaked some small parameters within battleship.js

--- battleship.js
+++ battleship.js
@@ -1,18 +1,14 @@
 /*===============================================*/
 
 const battleship = () => {
-    //assign name variables
-  let p1 = prompt("What is your name P1?");
-  let p2 = prompt("What is your name P2?");
-    //assign winner variable
+  const p1 = prompt("What is your name P1?");
+  const p2 = prompt("What is your name P2?");
   let winner;
-    //declare grid variable for p1
-  let p1Grid = createGrid(4);
-    //call function for random ship placement on p1 grid
+  
+  const p1Grid = createGrid(4);
   placeCharacters(1, p1Grid, 4);
-    //declare grid variable for p2
-  let p2Grid = createGrid(4);
-    //call function for random ship placement on p2 grid
+
+  const p2Grid = createGrid(4);
   placeCharacters(1, p2Grid, 4);
 
   //initialize players object
@@ -35,7 +31,7 @@ const battleship = () => {
 
   //1. create function that takes a size parameter to create a grid
   function createGrid(size) {
-    let grid = [];
+    const grid = [];
     for (i = 0; i < size; i++) {
       grid[i] = [];
       for (j = 0; j < size; j++) {
@@ -49,8 +45,8 @@ const battleship = () => {
   //2. create a function to generate random coordinates for ships and place them as 1's on the grid
   function placeCharacters(c, grid, max) {
     for (i = 0; i < max; i++) {
-      let randX = Math.floor(Math.random() * 4);
-      let randY = Math.floor(Math.random() * 4);
+      const randX = Math.floor(Math.random() * max);
+      const randY = Math.floor(Math.random() * max);
       if (grid[randX][randY] !== " 1 ") {
         grid[randX][randY] = ` ${c} `;
       }
@@ -59,12 +55,12 @@ const battleship = () => {
 
   //3. create a strike function to pass coordinates through opponent grid
   function strike(objV, player, opPlayer) {
-    let x = parseInt(
+    const x = parseInt(
       prompt(
         `${objV[player].name} choose an X coordinate to strike ${objV[opPlayer].name}'s battleship (0-3).`
       )
     );
-    let y = parseInt(
+    const y = parseInt(
       prompt(
         `${objV[player].name} choose a Y coordinate to strike ${objV[opPlayer].name}'s battleship (0-3).`
       )
@@ -96,7 +92,7 @@ const battleship = () => {
   //4. create function to start gameplay
   function start(objV, player, opPlayer) {
     while (objV[player].shipCount > 0 && objV[opPlayer].shipCount > 0) {
-      let attack = strike(objV, player, opPlayer);
+      const attack = strike(objV, player, opPlayer);
       if (attack === false) {
         attack = strike(objV, opPlayer, player);
       } 
